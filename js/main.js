6039297e9c891748d2b1b7d39e003c42237a3990
Daniela Maria Volletti <danielavolletti@gmail.com>
2020-04-22 12:16:23 +0200

    creazione grezza chiamata ajax anche per serie tv

--- js/main.js
+++ js/main.js
@@ -42,7 +42,8 @@ $(document).ready(function(){
             "titolo": singoloFilm.title,
             "titoloOriginale": singoloFilm.original_title,
             "lingua": singoloFilm.original_language,
-            "voto": singoloFilm.vote_average
+            "voto": singoloFilm.vote_average,
+            "tipo": "Film"
           };
           var html = template(context);
           // stampo in pagina i film
@@ -55,7 +56,46 @@ $(document).ready(function(){
         alert("Chiamata fallita!!!");
       }
 
-    })
+    });
+
+    // Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
+    $.ajax({
+      url: "https://api.themoviedb.org/3/search/tv",
+      method: "GET",
+      data: {
+        api_key: "d038cec7530a2aa194a0bd6917afb94d",
+        dataType: "json",
+        language: "it-IT",
+        query: valoreInput
+      },
+      success: function(risultato,stato){
+        // salvo in una variabile i film
+        var serie = risultato.results;
+
+        //ciclo i film ed estrapolo ciò che mi serve per la stampa in pagina
+        for (var i = 0; i < serie.length; i++) {
+          var singolaSerie= serie[i];
+
+          var context = {
+            "titolo": singolaSerie.name,
+            "titoloOriginale": singolaSerie.original_name,
+            "lingua": singolaSerie.original_language,
+            "voto": singolaSerie.vote_average,
+            "tipo": "Serie tv"
+          };
+          var html = template(context);
+          // stampo in pagina i film
+          $('.risultato-ricerca').append(html);
+
+        };
+
+      },
+      error: function(richiesta,stato,errore){
+        alert("Chiamata fallita!!!");
+      }
+
+    });
+
 
   })
 
